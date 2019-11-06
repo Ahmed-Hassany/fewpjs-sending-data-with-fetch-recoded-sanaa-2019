4bb356d149607ca456a015596877de2e078c31e2
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-11-06 09:17:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function submitData(uName,uEmail)
     return res.json();
   })
   .then(function(obj){
-    document.body.innerHTML = document.body.innerHTML+obj.id;
+    document.body.innerHTML = document.body.innerHTML+obj.id+"";
   })
   .catch(function(e){
     document.body.innerHTML = document.body.innerHTML+e.message;
